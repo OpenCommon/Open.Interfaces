c4d5f8b2d61d1110e0db54be8e9253315c52e674
OpenCommon <opencoder@126.com>
2025-02-09 18:45:41 +0800

    1.0.2

---
+++ Open.Interfaces/OrderAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Open.Interfaces;
+
+public class OrderAttribute : Attribute
+{
+    public OrderAttribute()
+    {
+
+    }
+
+    public OrderAttribute(int order)
+    {
+        Order = order;
+    }
+
+    public int Order { get; set; }
+}
